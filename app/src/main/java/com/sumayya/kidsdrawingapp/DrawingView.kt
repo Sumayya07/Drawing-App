1ecfdebfc0cbbd8e230fde7618ebe687126eed09
Sumayya <Sumayyasiddiqui7173@gmail.com>
2022-01-28 12:02:52 +0530

    Initial Commit

--- app/src/main/java/com/sumayya/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/sumayya/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.sumayya.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import java.util.ArrayList
@@ -92,6 +93,12 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
 
         invalidate()
         return true
+    }
+
+    fun setSizeForBrush(newSize: Float){
+        mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
+
     }
 
 
